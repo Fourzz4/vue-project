36b4e652395e2fe9761d2fc23134763b463b2f55
wczwy <450785343@qq.com>
2023-06-11 21:18:23 +0800

    test 1

--- src/http/axios.js
+++ src/http/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useUserStore } from "../store/user";
 //创建axios实例
 const instance=axios.create({
-    baseURL:'http://mall.qzimp.cn/',
+    baseURL:'https://mall.qzimp.cn/',
     timeout:6000
 })
 //请求拦截器
